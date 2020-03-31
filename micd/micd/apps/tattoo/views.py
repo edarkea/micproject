70ef2d8b87ca0e89765e063684a8bfd5cc2cc484
edarkea <edison.ayui@gmail.com>
2020-03-31 18:41:27 -0500

    Listar tattos y agregar, bug en la imagen

--- micd/micd/apps/tattoo/views.py
+++ micd/micd/apps/tattoo/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from micd.apps.tattoo.models import Tattoo
+from micd.apps.tattoo.forms import TattooForm
 
 # Create your views here.
 
 class TattooList(ListView):
     model = Tattoo
     template_name = 'tattoo_list.html'
+
+class ProductCreate(CreateView):
+    model = Tattoo
+    form_class = TattooForm
+    template_name = 'tattoo_form.html'
+    success_url = reverse_lazy('list_tattoo')
